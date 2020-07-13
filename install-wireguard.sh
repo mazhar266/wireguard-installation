ce085b45118a51299bcd0fec2f00f8f949c2dee5
Mazhar Ahmed <mazhar266@gmail.com>
2020-07-13 15:19:10 +0600

    Update install-wireguard.sh

--- install-wireguard.sh
+++ install-wireguard.sh
@@ -8,7 +8,9 @@ sudo add-apt-repository -y ppa:wireguard/wireguard
 sudo apt-get update
 sudo apt-get install -y wireguard
 
+# new directory for files
+mkdir wire && cd wire
+
 # generate keys
 umask 077  # This makes sure credentials don't leak in a race condition.
-wg genkey | tee privatekey | wg pubkey > publickey
-
+wg genkey | tee master.key | wg pubkey > master.pub.key
